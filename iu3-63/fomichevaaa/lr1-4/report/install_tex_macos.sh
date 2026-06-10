e802b705e9513bea89f20c875b1b32e401a451ae
Fomicheva Anna <fomicheva124@mail.ru>
2026-06-10 23:02:56 +0300

    удалить

--- iu3-63/fomichevaaa/lr1-4/report/install_tex_macos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-if ! command -v brew >/dev/null 2>&1; then
-    echo "Homebrew не найден. Сначала установите Homebrew: https://brew.sh" >&2
-    exit 1
-fi
-
-env HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask mactex-no-gui
